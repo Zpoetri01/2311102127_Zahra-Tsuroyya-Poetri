d18a53997cdb6b0ceebb799260ece236a385a2b7
Zpoetri01 <162399144+Zpoetri01@users.noreply.github.com>
2024-03-11 05:57:12 +0700

    guided1.cpp

--- pertemuan01/guided1.cpp
+++ pertemuan01/guided1.cpp
@@ -1,46 +1,47 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
-int main()
+#include <iostream> // untuk menjalankan operasi input dan output
+#include <iomanip> // untuk mengontrol format output data 
+using namespace std; // agar cout dan cin tidak memerlukan std::
+int main() 
 {
-    char op;
-    float num1, num2;
-    // It allows user to enter operator i.e. +, -, *, /
+    char op_zahra; // mendeklarasikan variabel 'op' dengan tipe data char 
+    float num1_127, num2_127; // mendeklarasikan variabel 'num1' & 'num2' dengan tipe data float
+    // pengguna dapat menginputkan operator
     cout << "Enter operator (+, -, *, /): ";
-    cin >> op;
-    // It allows user to enter the operands
+    cin >> op_zahra;
+    // pengguna dapat menginputkan dua operand 
     cout << "Enter two operands: ";
     cin >> num1 >> num2;
-    // Switch statement begins
-    switch (op)
+    // Menggunakan statement switch 
+    switch (op_zahra)
     {
-    // If user enters +
+    // jika pengguna memasukkan +
     case '+':
-        cout << "Result: " << num1 + num2;
-        break;
-    // If user enters -
+        cout << "Result: " << num1_127 + num2_127; // program akan menampilkan hasil dari penjumlahan num1 & num2
+        break; 
+    // jika pengguna memasukkan -
     case '-':
-        cout << "Result: " << num1 - num2;
+        cout << "Result: " << num1_127 - num2_127; // program akan menampilkan hasil dari pengurangan num1 & num2
         break;
-    // If user enters *
+    // jika pengguna memasukkan *
     case '*':
-        cout << "Result: " << num1 * num2;
+        cout << "Result: " << num1_127 * num2_127; // program akan menampilkan hasil dari perkalian num1 & num2
         break;
-    // If user enters /
+    // jika pengguna memasukkan /
     case '/':
-        if (num2 != 0)
+        if (num2_127 != 0)
         {
-            cout << "Result: " << fixed << setprecision(2) << num1 / num2;
+            cout << "Result: " << fixed << setprecision(2) << num1_127 / num2_127; // program akan menampilkan hasil dari pembagian num1 & num2
+   dengan hasil apabila bilangan desimal tetap akan ditampilkan dengan dua angka dibelakang koma     
         }
         else
         {
-            cout << "Error! Division by zero is not allowed.";
+            cout << "Error! Division by zero is not allowed."; // hasil program jika memasukkan nol
         }
         break;
-    // If the operator is other than +, -, * or /,
-    // error message will display
+    // jika pengguna memasukkan operator selain  +, -, * or /,
+    // pesan kesalahan akan ditampilkan
     default:
         cout << "Error! Operator is not correct";
-    } // switch statement ends
-    return 0;
+    } // statement switch selesai
+    return 0; mengembalikan nilai nol 
 }
